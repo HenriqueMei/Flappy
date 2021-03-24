3dd9aa95e0258f64480891b3960975dc2ab96754
HenriqueMei <43459583+HenriqueMei@users.noreply.github.com>
2021-03-24 19:59:35 -0300

    Mei
    Projeto concluido

--- Flappy/Assets/flappy.cs
+++ Flappy/Assets/flappy.cs
@@ -19,7 +19,7 @@ public class Flappy : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetMouseButtonDown(0))
+        if (Input.GetMouseButtonDown(0))
         {
             //velocity adiciona velocidade
             //Vector2.up subir
